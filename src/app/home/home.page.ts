0d27c36da0b2aa9635bab91bb8eca644612da2ac
ponyhead2219 <20253L601101@gmail.com>
2026-09-23 12:09:54 -0600

    Cambios en la pagina numero 2

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,16 +1,43 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonTitle, IonContent, IonFooter, IonButton} from '@ionic/angular';
+import { IonHeader, IonTitle, IonContent, IonFooter, IonButton, IonIcon } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { caretUpOutline, caretDownOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonTitle, IonContent,IonFooter, IonButton]
+  imports: [IonButton, IonFooter, IonHeader, IonTitle, IonContent, IonIcon],
 })
 
-//Aquí va toda la logica de mi app movil
-export class HomePage 
-{
-  public numero: number =0;
-  constructor() {}
+// Aquí va toda la lógica de mi App Móvil
+// Métodos, Variables, etc.
+export class HomePage {
+  public numero: number = 0;
+
+  // Constantes
+  // readonly: Evita que el valor de mi constante cambie 
+  public readonly MINIMO: number = 0;
+  public readonly MAXIMO: number = 9;
+
+  constructor() {
+    // Registrar los íconos para poder usarlos
+    addIcons({ caretUpOutline, caretDownOutline });
+  }
+
+  counterUp() {
+    if (this.numero < this.MAXIMO) {
+      // console.log('Up');
+      // this.numero = this.numero + 1;
+      this.numero++;
+    }
+  }
+
+  counterDown() {
+    if (this.numero > this.MINIMO) {
+      // console.log('Down');
+      // this.numero = this.numero - 1;
+      this.numero--;
+    }
+  }
 }
